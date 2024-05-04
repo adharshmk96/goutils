653351ce0ad9b31f0ae175f5a8b7b2c3ab862463
Adharsh <debugslayer@gmail.com>
2024-05-05 06:07:39 +0700

    initial commit with password hashing util.

---
+++ password/hashing_test.go
@@ -0,0 +1,34 @@
+package password_test
+
+import (
+	"testing"
+
+	"github.com/adharshmk96/goutils/password"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHashPassword(t *testing.T) {
+	params := password.DefaultParams
+	hash, err := password.HashPassword("password", params)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, hash)
+
+	assert.NotEqual(t, "password", hash)
+}
+
+func TestVerifyPasswordHash(t *testing.T) {
+	params := password.DefaultParams
+	hash, err := password.HashPassword("password", params)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, hash)
+
+	match, err := password.VerifyPasswordHash("password", hash)
+	assert.NoError(t, err)
+	assert.True(t, match)
+
+	match, err = password.VerifyPasswordHash("wrongpassword", hash)
+	assert.NoError(t, err)
+	assert.False(t, match)
+}
