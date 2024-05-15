d2f6c3cf29d28b33bd18509ae2c16a521bd9df63
Adharsh M <48812285+adharshmk96@users.noreply.github.com>
2024-05-15 23:21:40 +0700

    rate limited http client added

---
+++ http/limiter.go
@@ -0,0 +1,24 @@
+type RateLimitedClient struct {
+	client  *http.Client
+	limiter *rate.Limiter
+}
+
+func NewRateLimitedClient(rps int) *RateLimitedClient {
+	return &RateLimitedClient{
+		client:  &http.Client{},
+		limiter: rate.NewLimiter(rate.Limit(rps), 1),
+	}
+}
+
+func (c *RateLimitedClient) Do(req *http.Request) (*http.Response, error) {
+	if err := c.limiter.Wait(req.Context()); err != nil {
+		return nil, err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
